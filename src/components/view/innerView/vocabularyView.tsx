c0dec466c070dcaf958ed8caf84f91a8f7889737
David <125945778+davidbucodes@users.noreply.github.com>
2024-02-20 14:20:48 +0800

    feat: vocabulry meaning columns

--- src/components/view/innerView/vocabularyView.tsx
+++ src/components/view/innerView/vocabularyView.tsx
@@ -64,7 +64,48 @@ export function VocabularyView({
           {Boolean(vocab.meaning) && (
             <Styles.Line>
               <b>Meaning:</b>{" "}
-              <ul>
+              <ul
+                style={{
+                  // marginBlockStart: 0,
+                  // marginBlockEnd: 0,
+                  // paddingInlineStart: fontSizes.small,
+                  // columns: 2,
+                  // gap: fontSizes.giant,
+
+                  // display: "grid",
+                  // gridTemplateRows: "repeat(5, min-content)",
+                  // gridAutoFlow: "column",
+                  // gap: "1em",
+
+                  // display: "grid",
+                  // gridTemplateColumns: "repeat(3, max-content)",
+                  // gridAutoFlow: "row",
+                  // gap: "0 2em",
+
+                  // display: "grid",
+                  // gridTemplateColumns: "repeat(auto-fit, 207px)",
+                  // gridAutoFlow: "row",
+                  // gap: "0px 2em",
+                  // flexGrow: "1",
+
+                  // flexGrow: 1,
+                  // display: "grid",
+                  // gridTemplateColumns: "repeat(auto-fit, 30%)",
+                  // gridAutoFlow: "row",
+                  // gap: "0 2em",
+
+                  // display: "flex",
+                  // flexFlow: "wrap column",
+                  // maxHeight: "150px",
+
+                  display: "flex",
+                  flexFlow: "column wrap",
+                  maxHeight: "265px",
+                  gap: "0 30px",
+                  overflow: "auto",
+                  flexGrow: "1",
+                }}
+              >
                 {vocab.meaning.map((m, index) => (
                   <li key={m + "" + index}>{m}</li>
                 ))}
