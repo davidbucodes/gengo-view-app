032452f383ebefb6432a8e1e7cc62ac7ff30826a
David <125945778+davidbucodes@users.noreply.github.com>
2024-09-28 17:11:46 +0900

    feat: update typescript version and styles

--- src/components/common/link/style.ts
+++ src/components/common/link/style.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 export namespace Styles {
-  export const Link = styled.div`
+  export const Link = styled.div<{ as?: string }>`
     cursor: pointer;
     vertical-align: middle;
+    ${props => (props.as === "tr" ? "display: flex;" : "")}
     &:hover,
     &:focus {
       div,
